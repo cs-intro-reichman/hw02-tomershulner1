f360db5236576d7b5bf8469a220667826fdb387f
tomer_shulner <tomer.shulner@innoviz-tech.com>
2023-12-26 18:28:32 +0200

    fix damaka

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -5,11 +5,14 @@ public class DamkaBoard {
 	public static void main(String[] args) {
 		int num_of_lines = Integer.parseInt(args[0]);
 		for (int i = 0; i < num_of_lines; i++) {
-			if (i % 2 != 0) {
-				System.out.print(" ");
-			}
 			for (int j = 0; j < num_of_lines; j++) {
-				System.out.print("* ");
+				if (i % 2 != 0) {
+					System.out.print(" *");
+				}
+				else {
+					System.out.print("* ");
+				}
+				
 			}
 			System.out.println();
 		}
