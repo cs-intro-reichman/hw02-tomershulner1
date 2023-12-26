82be7dcbe1dbdd502db688cdcdd347b90914a37a
tomer_shulner <tomer.shulner@innoviz-tech.com>
2023-12-26 21:56:10 +0200

    One of each works

--- OneOfEach.java
+++ OneOfEach.java
@@ -5,6 +5,21 @@
  */
 public class OneOfEach {
 	public static void main (String[] args) {
-		//// Put your code here
+		Boolean have_boy = false;
+		Boolean have_girl = false;
+		int all_kids = 0;
+		while (!have_boy || !have_girl) {
+			double chance = Math.random();
+			if (chance > 0.5) {
+				System.out.print("g ");
+				have_girl = true;
+			}
+			else {
+				System.out.print("b ");
+				have_boy = true;
+			}
+			all_kids += 1;
+		}
+		System.out.println("\nYou made it... and you now have " + all_kids + " children.");
 	}
 }
